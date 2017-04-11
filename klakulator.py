aadc3dd9da5546c258677f090ff5c52db3098449
Kasia <wydro.kasia@gmail.com>
2017-04-11 19:25:28 +0200

    funkcja z pomoca gotowa do review

--- klakulator.py
+++ klakulator.py
@@ -3,5 +3,9 @@ def dodawanie():
 	a = int(input('Podaj pierwsza liczbe'))
 	b = int(input('Podaj druga liczbe'))
 	print(a+b)
-	
+
+def get_help():
+	print("To jest prosty program kalkulatora. Wprowadź dwie liczby i zatwierdź enterem.")
+
+get_help()
 dodawanie()
